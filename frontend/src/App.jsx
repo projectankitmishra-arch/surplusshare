1f716ac017ceab541e9795a7ebefa6ec55e92fe8
AnkitMishra1905 <ankitmishra1905@gmail.com>
2026-06-07 23:53:25 +0530

    Added protected routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,21 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import PostFood from "./pages/PostFood";
 import MyListings from "./pages/MyListings";
+import { useAuth } from "./context/AuthContext";
+
+function ProtectedRoute({ children }) {
+  const { user } = useAuth();
+  return user ? children : <Navigate to="/login" />;
+}
 
 function App() {
   return (
@@ -15,8 +26,24 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
-          <Route path="/post" element={<PostFood />} />
-          <Route path="/my-listings" element={<MyListings />} />
+
+          <Route
+            path="/post"
+            element={
+              <ProtectedRoute>
+                <PostFood />
+              </ProtectedRoute>
+            }
+          />
+
+          <Route
+            path="/my-listings"
+            element={
+              <ProtectedRoute>
+                <MyListings />
+              </ProtectedRoute>
+            }
+          />
         </Routes>
       </div>
     </Router>
